178702e82360fdfb18f4595ea2d6ce5560c041a5
Henri Björkman <henri.bjorkman@protacon.com>
2015-11-05 16:09:38 +0200

    Fetch multiple values at one time Collect wanted values from config Allow defining config file from commandline

--- main.py
+++ main.py
@@ -3,20 +3,39 @@
 import obd
 import signal
 import sys
+import os
+import configparser
+import argparse
+import time
+
+#Parse arguments
+argparser = argparse.ArgumentParser(description="Collect OBD2 data and send it to Firebase")
+argparser.add_argument('-c', '--config', default='obd2logger.conf')
+args = argparser.parse_args()
+
+#Get settings from configfile
+config = configparser.ConfigParser()
+config.optionxform = str
+try:
+	config.read(args.config)
+except(Exception, e):
+	print(e)
 
 #What to do when we receive a signal
 def signal_handler(signal, frame):
-    connection.close()
-    sys.exit(0)
+	connection.close()
+	sys.exit(0)
 	
 #Register our signal handler
 signal.signal(signal.SIGINT, signal_handler)
 
-#Find and connect OBD adapter
-connection = obd.OBD()
+#Connect OBD adapter
+connection = obd.OBD(config.get('Connection', 'serial_port'))
 
 while True:
-    request = connection.query(obd.commands.RPM)
-
-    if not request.is_null():
-        print(request.value)
+	for command, value in config.items('Collection'):
+		if value == "1":
+			request = connection.query(obd.commands[command])
+			if not request.is_null():
+				print(request.value)
+	time.sleep(1)
