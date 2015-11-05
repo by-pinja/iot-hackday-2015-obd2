a72468f6988ba3fc5f815b68a07c990809f80864
Henri Björkman <henri.bjorkman@protacon.com>
2015-11-05 13:25:13 +0200

    Handle ctrl+c with signal Fix more typos

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 #ODB2 datalogger
 
 import obd
+import signal
+import sys
 
+#What to do when we receive a signal
+def signal_handler(signal, frame):
+    connection.close()
+    sys.exit(0)
+	
+#Register our signal handler
+signal.signal(signal.SIGINT, signal_handler)
+
+#Find and connect OBD adapter
 connection = obd.OBD()
 
-while true:
+while True:
     request = connection.query(obd.commands.RPM)
 
-    if not r.is_null():
-        print(r.value)
+    if not request.is_null():
+        print(request.value)
